5c91ae46e612d3749eb64ddfae70a81bfe9fe079
derekbache <derek.bache@gmail.com>
2023-03-18 14:35:00 -0700

    Make square size dynamic based on size of fixed container, add button to adjust grid size

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 const container = document.querySelector(".container");
 let gridSize = 16;
 
-for (i = 0; i < gridSize; i++) {
-  const flexContainer = document.createElement("div");
-  flexContainer.className = "flexContainer";
-  container.appendChild(flexContainer);
+drawGrid(gridSize);
+
+function drawGrid(gridSize) {
+  for (i = 0; i < gridSize; i++) {
+    const flexContainer = document.createElement("div");
+    flexContainer.className = "flexContainer";
+    container.appendChild(flexContainer);
+  }
+
+  const flexContainerNodeList = document.querySelectorAll("div.flexContainer");
+
+  for (i = 0; i < gridSize; i++) {
+    for (const container of flexContainerNodeList) {
+      const gridSquare = document.createElement("div");
+      gridSquare.className = "gridSquare";
+      gridSquare.style.height = 1000 / gridSize + "px";
+      gridSquare.style.width = 1000 / gridSize + "px";
+      container.appendChild(gridSquare);
+    }
+  }
+
+  const squares = document.querySelectorAll("div.gridSquare");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.classList.add("hover");
+    });
+  });
 }
 
-const flexContainerNodeList = document.querySelectorAll("div.flexContainer");
-console.log(flexContainerNodeList);
-
-for (i = 0; i < gridSize; i++) {
-  for (const container of flexContainerNodeList) {
-    const gridSquare = document.createElement("div");
-    gridSquare.className = "gridSquare";
-    gridSquare.style.width = 100 / gridSize + "%";
-    gridSquare.style.height = "50px";
-    container.appendChild(gridSquare);
+function removeGrid() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
 }
 
-const squares = document.querySelectorAll("div.gridSquare");
-
-console.log(squares);
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.classList.add("hover");
-  });
-});
+function gridSizeChange() {
+  let gridSizeRequest;
+  do {
+    gridSizeRequest = prompt(
+      "Please enter desired grid size between 0 and 101",
+      "16"
+    );
+  } while (gridSizeRequest <= 0 || gridSizeRequest > 100);
+  removeGrid();
+  drawGrid(gridSizeRequest);
+}
